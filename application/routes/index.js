9f8d6ece99ec2998cfa46ee0c745f4d48150d13b
Toshi <leonardotkimura@gmail.com>
2021-03-31 11:12:23 -0300

    add link login logout

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const InsertCertificateService = require('./../services/insertCertificateService')
 
-router.use('/', require('./homeRoute'));
 router.use('/session', require('./sessionRoute'));
 
 router.use(requireCertificate)
+router.use('/', require('./homeRoute'));
 router.use('/data', require('./dataRoute'));
 router.use('/processor', require('./processorRoute'));
 router.use('/operation', require('./operationRoute'));
