1e1a7edb2ac0cc4dd0b05aa70913c91d16198518
Toshi <leonardotkimura@gmail.com>
2021-09-09 17:56:59 -0300

    sidenav active

--- application/public/js/document.js
+++ application/public/js/document.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
+
+  // input biocoins field mask
   $('.bio-money-input').keypress(function(){
     this.value = parseFloat(this.value.replace(/(.*){1}/, '0$1').replace(/[^\d]/g, '').replace(/(\d\d\d\d\d\d\d\d?)$/, '.$1')).toFixed(8);
-});
+  });
 
+  // sidebar active behavior
+  var pathname = window.location.pathname
+  const active = $('.navbar-nav > li a[href="' + pathname + '"]').parents('li')
+  active.addClass('active');
 })
 
 
