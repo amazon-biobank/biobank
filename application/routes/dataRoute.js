c45acb9b9009e6fac9762ae4df5c0e239ae69a4e
Gustav <59322464+GustavsC@users.noreply.github.com>
2022-08-04 11:49:55 -0300

    Update dataRoute.js

--- application/routes/dataRoute.js
+++ application/routes/dataRoute.js
@@ -4,10 +4,8 @@ var DataController = require('../controllers/dataController')
 router.get('/', DataController.index);
 router.get('/raw-data-new', DataController.newRawData);
 router.get('/processed-data-new', DataController.newProcessedData);
-router.get('/tradicional-knowledge-new', DataController.newTradicionalKnowledgeData) //Arrumar aqui 
 router.post('/raw-data-create', DataController.createRawData);
 router.post('/processed-data-create', DataController.createProcessedData);
-router.post('/tradicional-knowledge-create', DataController.createTradicionalKnowledgeData) //Arrumar aqui 
 
 router.get('/:dataId', DataController.show);
 router.get('/:dataId/list-operations', DataController.listOperations);
