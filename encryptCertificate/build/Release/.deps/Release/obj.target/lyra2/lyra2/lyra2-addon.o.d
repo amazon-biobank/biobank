85ac44d965103b6ad40f57848c305f98f7461686
Breno Suguru <suguru.ben@usp.br>
2022-08-31 13:26:21 -0300

    :ambulance: Fixed certificate decryption with lyra2; works with certificates encrypted in AmazoniaWeb

--- encryptCertificate/build/Release/.deps/Release/obj.target/lyra2/lyra2/lyra2-addon.o.d
+++ encryptCertificate/build/Release/.deps/Release/obj.target/lyra2/lyra2/lyra2-addon.o.d
@@ -1,23 +1,23 @@
-cmd_Release/obj.target/lyra2/lyra2/lyra2-addon.o := g++ -o Release/obj.target/lyra2/lyra2/lyra2-addon.o ../lyra2/lyra2-addon.cc '-DNODE_GYP_MODULE_NAME=lyra2' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/local/home/hyper/.cache/node-gyp/15.14.0/include/node -I/local/home/hyper/.cache/node-gyp/15.14.0/src -I/local/home/hyper/.cache/node-gyp/15.14.0/deps/openssl/config -I/local/home/hyper/.cache/node-gyp/15.14.0/deps/openssl/openssl/include -I/local/home/hyper/.cache/node-gyp/15.14.0/deps/uv/include -I/local/home/hyper/.cache/node-gyp/15.14.0/deps/zlib -I/local/home/hyper/.cache/node-gyp/15.14.0/deps/v8/include -I../node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/lyra2/lyra2/lyra2-addon.o.d.raw   -c
+cmd_Release/obj.target/lyra2/lyra2/lyra2-addon.o := g++ -o Release/obj.target/lyra2/lyra2/lyra2-addon.o ../lyra2/lyra2-addon.cc '-DNODE_GYP_MODULE_NAME=lyra2' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/stefanini/.cache/node-gyp/14.20.0/include/node -I/home/stefanini/.cache/node-gyp/14.20.0/src -I/home/stefanini/.cache/node-gyp/14.20.0/deps/openssl/config -I/home/stefanini/.cache/node-gyp/14.20.0/deps/openssl/openssl/include -I/home/stefanini/.cache/node-gyp/14.20.0/deps/uv/include -I/home/stefanini/.cache/node-gyp/14.20.0/deps/zlib -I/home/stefanini/.cache/node-gyp/14.20.0/deps/v8/include -I../node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/lyra2/lyra2/lyra2-addon.o.d.raw   -c
 Release/obj.target/lyra2/lyra2/lyra2-addon.o: ../lyra2/lyra2-addon.cc \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/node.h \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8.h \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/cppgc/common.h \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8config.h \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8-internal.h \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8-version.h \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8config.h \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8-platform.h \
- /local/home/hyper/.cache/node-gyp/15.14.0/include/node/node_version.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/node.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/v8.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/cppgc/common.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/v8config.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/v8-internal.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/v8-version.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/v8config.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/v8-platform.h \
+ /home/stefanini/.cache/node-gyp/14.20.0/include/node/node_version.h \
  ../lyra2/LyraHash.h
 ../lyra2/lyra2-addon.cc:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/node.h:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8.h:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/cppgc/common.h:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8config.h:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8-internal.h:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8-version.h:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8config.h:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/v8-platform.h:
-/local/home/hyper/.cache/node-gyp/15.14.0/include/node/node_version.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/node.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/v8.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/cppgc/common.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/v8config.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/v8-internal.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/v8-version.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/v8config.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/v8-platform.h:
+/home/stefanini/.cache/node-gyp/14.20.0/include/node/node_version.h:
 ../lyra2/LyraHash.h:
---
+++ encryptCertificate/src/decryptCredentials.js
@@ -0,0 +1,59 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+const lyra2 = require ('../build/Release/lyra2');
+const LyraHash = (input, salt, saltLength) => lyra2.getHash(input, salt, saltLength);
+
+const algorithm = 'aes-256-cfb';
+const password = 'teste'; //password used to generate the ciphered certificate
+
+const KEY_SIZE = 32;
+const SALT_LENGTH = 16;
+
+const ArrayBufferToBuffer = (arrayBuffer) => {
+  const buffer = Buffer.from(arrayBuffer)
+
+  return buffer;
+}
+
+const splitKeyIv = (keyHash) => {
+  const bufferKey = ArrayBufferToBuffer(keyHash);
+  const key = bufferKey.slice(0, KEY_SIZE);
+  const iv = bufferKey.slice(KEY_SIZE, KEY_SIZE  + KEY_SIZE / 2);
+
+  return [key, iv];
+}
+
+const getCipheredCertificate = () => { //ciphered certificate in local folder
+  const jsonString = fs.readFileSync('../credentials/encryptedCredentials.json')
+  const credentials = JSON.parse(jsonString)
+  return [credentials.encrypted_content, credentials.salt]
+  
+}
+
+const generateHash = (password, salt) => {
+  const passwordHash = LyraHash(password, salt, SALT_LENGTH);
+
+  return passwordHash;
+}
+
+const decrypt = (passwordHash, encrypted_content) => {
+  const base64Cypher = Buffer.from(encrypted_content, 'base64');
+  const [key, iv] = splitKeyIv(passwordHash);
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  const decrypted = decipher.update(base64Cypher, '', 'utf-8');
+
+  return decrypted;
+}
+
+const writeDecrypted = (decrypted) => {
+  fs.writeFile("../credentials/decryptedCredentials.json", decrypted, function(err, result) {
+    if(err) console.log('error', err);
+  }); 
+}
+
+const [encrypted_content, salt] = getCipheredCertificate(); //encrypted certificate and salt used to hash password
+const passwordHash = generateHash(password, salt);
+const decrypted = decrypt(passwordHash, encrypted_content);
+writeDecrypted(decrypted);
+
