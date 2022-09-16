d765cf04a2fea7491aeee24d74920187036d2e0a
hyper <hyper>
2022-09-16 22:18:30 +0000

    removing encryptCertificate/build

--- encryptCertificate/build/Release/.deps/Release/lyra2.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/lyra2.node := ln -f "Release/obj.target/lyra2.node" "Release/lyra2.node" 2>/dev/null || (rm -rf "Release/lyra2.node" && cp -af "Release/obj.target/lyra2.node" "Release/lyra2.node")
--- encryptCertificate/build/Release/.deps/Release/obj.target/lyra2.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/lyra2.node := g++ -o Release/obj.target/lyra2.node -shared -pthread -rdynamic -m64  -Wl,-soname=lyra2.node -Wl,--start-group Release/obj.target/lyra2/lyra2/Lyra2.o Release/obj.target/lyra2/lyra2/LyraHash.o Release/obj.target/lyra2/lyra2/Sponge.o Release/obj.target/lyra2/lyra2/lyra2-addon.o -Wl,--end-group 
