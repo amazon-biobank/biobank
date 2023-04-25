287d93d523bebe5c183826278917c5930acb55dd
Ubuntu User <ubuntu@biobank-setup-test.cs3b7cloud.internal>
2023-04-25 14:24:30 +0000

    add bin and config to gitignore

--- configure-first-use.sh
+++ configure-first-use.sh
@@ -1,19 +1,21 @@
-cd "../currency-contract/"
-npm install
-cd -
 
-cd "../biobank-contract/"
-npm install
-cd -
 
-cd ./../../api
-npm install
-cd -
+# cd "../currency-contract/"
+# npm install
+# cd -
 
-cd ./../../application/
-npm install
-cd - 
+# cd "../biobank-contract/"
+# npm install
+# cd -
 
-cd ./../../../keyguard/
-npm install
-cd -
+# cd ./../../api
+# npm install
+# cd -
+
+# cd ./../../application/
+# npm install
+# cd - 
+
+# cd ./../../../keyguard/
+# npm install
+# cd -
